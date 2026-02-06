a0d06cb7a43532718b009b93a785872ede9469d4
samarth <samarthkashyap03@gmail.com>
2026-02-06 23:25:54 +0100

    update: day-48 auto login and booking.

--- days/day-48/gym-booking-automation/main.py
+++ days/day-48/gym-booking-automation/main.py
@@ -3,46 +3,53 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 import os,time,json
+from dotenv import load_dotenv
+load_dotenv()
+USER_NAME=os.getenv('ACCOUNT_EMAIL')
+PASSWORD=os.getenv('ACCOUNT_PASSWORD')
+#Create a directory to save profile information
+user_profile=os.path.join(os.getcwd(),'user_profile')
+os.makedirs(user_profile,exist_ok=True)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument(f"--user-data-dir={user_profile}")
 
 driver=webdriver.Chrome(options=chrome_options)
 driver.get('https://appbrewery.github.io/gym/')
 
-#Create a directory to save profile information
-user_profile=os.path.join(os.getcwd(),'user_profile')
-os.makedirs(user_profile,exist_ok=True)
-text_file=os.path.join(user_profile,'profile_info.txt')
-
 #Click login button
 login=driver.find_element(By.CSS_SELECTOR,'div.Navigation_rightSection__L2XbA button')
-time.sleep(1)
+driver.implicitly_wait(1)
 login.click()
 
-try:
-    f=open(f"{os.getcwd()}\\user_profile\\profile_info.txt",'r')
-except FileNotFoundError:
-    f=open(f"{os.getcwd()}\\user_profile\\profile_info.txt",'w')
-time.sleep(1)
-
-if f.read()=="":
-    print("Please register")
+if USER_NAME=="" or PASSWORD=="":
+    print("Please set your email and password in the .env file.")
     #Clicks on register button
     register=driver.find_element(By.CSS_SELECTOR,'div.Login_toggleContainer__8LQrF button')
     register.click()
-    #Waits for user input
-    name_input=driver.find_element(By.CSS_SELECTOR,'div.Login_formGroup__jFLXd input')
-    WebDriverWait(driver,10).until(lambda d:name_input.get_attribute('value'))
-    name=name_input.get_attribute('value')
-    time.sleep(5)
-    with open(f"{os.getcwd()}\\user_profile\\profile_info.txt",'a') as f:
-        json.dump({"Name":name},f)
-    email_input=driver.find_element(By.CSS_SELECTOR,'div.Login_formGroup__jFLXd #email-input')
-    email_input.click()
-    WebDriverWait(driver,10).until(lambda d:email_input.get_attribute('value'))
-    email=email_input.get_attribute('value')
-    time.sleep(5)   
-    with open(f"{os.getcwd()}\\user_profile\\profile_info.txt",'a') as f:
-        json.update({"Email":email},f)
+else:
+    #Fill email and password in login fields
+    email=driver.find_element(By.CSS_SELECTOR,'div.Login_formGroup__jFLXd input#email-input')
+    email.click()
+    email.send_keys(USER_NAME)
+    driver.implicitly_wait(1)
+    try:
+        password_box=driver.find_element(By.CSS_SELECTOR,'div.Login_formGroup__jFLXd input#password-input')
+    except NoSuchElementException:
+        print('Layout has been changed')
+    else:
+        password_box.click()
+        password_box.send_keys(PASSWORD)
+        driver.implicitly_wait(1)
+    #Log-in
+    submit=driver.find_element(By.CSS_SELECTOR,'form#login-form button#submit-button')
+    submit.click()
+    driver.implicitly_wait(1)
+
+    card=driver.find_elements(By.CSS_SELECTOR,'div[id^="day-group-tue-"]')
+    print(card)
+
+
+   
          
